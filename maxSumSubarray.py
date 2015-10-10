7c8b8925e28727efe414ec300efcfa8062f3e902
watsokel <watsokel@onid.oregonstate.edu>
2015-10-09 19:58:22 -0700

    added gitignore file, created maxSumSubarray.py

---
+++ maxSumSubarray.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#title          :maxSumSubarray.py
+#description    :finds the subarray with the maximum sum
+#author         :project group 2: Joseph Barlan, Jeff Mueller, Kelvin Watson
+#creation date  :9 October 2015
+#last modified  :9 October 2015
+#usage          :python maxSumSubarray.py
+#notes          :
+#python_version :2.6.6
+#==============================================================================

608bf3a755d7536e1579ca7eececcd31156f8bde
watsokel <watsokel@onid.oregonstate.edu>
2015-10-11 00:37:30 -0700

    currently algorithm 2,3,4 fail on [-2,-3,-4,-5...] type arrays

--- maxSumSubarray.py
+++ maxSumSubarray.py
@@ -9,9 +9,26 @@
 #python_version :2.6.6
 #==============================================================================
 
+
 #Algorithm 1:
 
-def brute_force_enumeration(array):
+def enumeration(array):
+    left = right = 0
+    max_sum = array[0] #I added this line, else this kind of array fails [-2,-3,-4]
+    for i,x in enumerate(array):
+        for j,y in enumerate(array[i+1:],i+1):
+            temp_sum=0
+            for k,z in enumerate(array[i:j+1],i):
+                temp_sum = temp_sum + z #accumulate sum for this pair
+            if temp_sum > max_sum:
+                max_sum = temp_sum
+                left = i
+                right = j
+    return left,right,max_sum
+
+#Algorithm 2:
+
+def better_enumeration(array):
     left = right = best_right = curr_best_sum = temp_sum = 0
     for i,v in enumerate(array):
         temp_sum = loop_sum = array[i]
@@ -25,19 +42,6 @@ def brute_force_enumeration(array):
             left = i
             best_right = right
     return (left,best_right,curr_best_sum)
-
-#Algorithm 2:
-
-def better_enumeration(array):
-	curr_best_sum = temp_sum = array[0]
-	left = right = 0
-	for i,v in enumerate(array[1:]):
-		temp_sum = max(array[1:][i], temp_sum+array[1:][i])
-		#NOTE: temp_sum is really temp_sum from left to i-1
-		if(temp_sum == array[1:][i]): left=i+1
-		curr_best_sum = max(curr_best_sum, temp_sum)
-		if(curr_best_sum == temp_sum): right=i+1
-	return (left,right,curr_best_sum)
 
 #Algorithm 3: Divide and Conquer
 
@@ -87,17 +91,48 @@ def max_subarray_recursive(array, low, high):
             return (cross_low, cross_high, cross_sum)
 
 
+
+#Algorithm 4:
+
+def linear_time(array):
+	curr_best_sum = temp_sum = array[0]
+	left = right = 0
+	for i,v in enumerate(array[1:]):
+		temp_sum = max(array[1:][i], temp_sum+array[1:][i])
+		#NOTE: temp_sum is really temp_sum from left to i-1
+		if(temp_sum == array[1:][i]): left=i+1
+		curr_best_sum = max(curr_best_sum, temp_sum)
+		if(curr_best_sum == temp_sum): right=i+1
+	return (left,right,curr_best_sum)
+
+
 #TEST SCRIPT
 
-vals=[10,8,-255,-30,50,30050,8,50,-77,76,-75]
+vals1=[10,8,-255,-30,50,30050,8,50,-77,76,-75]
 vals2=[-2,1,8,2,-6,5,105]
-print("---TESTING ALGORITHM 1---")
-print(str(brute_force_enumeration(vals)))
-print(str(brute_force_enumeration(vals2)))
-print("---TESTING ALGORITHM 2---")
-print(str(better_enumeration(vals)))
-print(str(better_enumeration(vals2)))
-print("---TESTING ALGORITHM 3---")
-print(str(max_subarray_recursive(vals,0,10)))
-print(str(max_subarray_recursive(vals2,0,6)))
+vals3=[-2,-3,-4,-5,-6,-7,-8,-9,-10]
+vals4=[10,20,30,40,50]
 
+print("---TESTING ALGORITHM 1---")
+print(str(enumeration(vals1)))
+print(str(enumeration(vals2)))
+print(str(enumeration(vals3)))
+print(str(enumeration(vals4)))
+
+print("---TESTING ALGORITHM 2---")
+print(str(better_enumeration(vals1)))
+print(str(better_enumeration(vals2)))
+print(str(better_enumeration(vals3)))
+print(str(better_enumeration(vals4)))
+
+print("---TESTING ALGORITHM 3---")
+print(str(max_subarray_recursive(vals1,0,10)))
+print(str(max_subarray_recursive(vals2,0,6)))
+print(str(max_subarray_recursive(vals3,0,8)))
+print(str(max_subarray_recursive(vals4,0,4)))
+
+print("---TESTING ALGORITHM 4---")
+print(str(linear_time(vals1)))
+print(str(linear_time(vals2)))
+print(str(linear_time(vals3)))
+print(str(linear_time(vals4)))

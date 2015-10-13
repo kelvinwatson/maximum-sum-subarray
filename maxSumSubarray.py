3dc61db62c9744e9e9e54a5b3da50d62e7a707a1
watsokel <watsokel@onid.oregonstate.edu>
2015-10-12 20:30:45 -0700

    corrected algorithm 4

--- maxSumSubarray.py
+++ maxSumSubarray.py
@@ -3,7 +3,7 @@
 #description    :finds the subarray with the maximum sum
 #author         :project group 2: Joseph Barlan, Jeff Mueller, Kelvin Watson
 #creation date  :9 October 2015
-#last modified  :9 October 2015
+#last modified  :11 October 2015
 #usage          :python maxSumSubarray.py
 #notes          :
 #python_version :2.6.6
@@ -95,16 +95,28 @@ def max_subarray_recursive(array, low, high):
 #Algorithm 4:
 
 def linear_time(array):
-	curr_best_sum = temp_sum = array[0]
-	left = right = 0
-	for i,v in enumerate(array[1:]):
-		temp_sum = max(array[1:][i], temp_sum+array[1:][i])
-		#NOTE: temp_sum is really temp_sum from left to i-1
-		if(temp_sum == array[1:][i]): left=i+1
-		curr_best_sum = max(curr_best_sum, temp_sum)
-		if(curr_best_sum == temp_sum): right=i+1
-	return (left,right,curr_best_sum)
-
+	# curr_best_sum = temp_sum = array[0]
+	# left = right = 0
+	# for i,v in enumerate(array[1:]):
+	# 	temp_sum = max(array[1:][i], temp_sum+array[1:][i])
+	# 	#NOTE: temp_sum is really temp_sum from left to i-1
+	# 	if(temp_sum == array[1:][i]): left=i+1
+	# 	curr_best_sum = max(curr_best_sum, temp_sum)
+	# 	if(curr_best_sum == temp_sum): right=i+1
+	# return (left,right,curr_best_sum)
+    currBestSum = tempSum = tempLeft = left = 0
+    right = -1
+    for i,v in enumerate(array):
+        if (tempSum + array[i]) < 0: #everything before is trumped
+            tempLeft=i+1
+            tempSum=0 #trash previous values and start counting from here!
+        else:	#adding the array[i] to tempSum makes tempSum larger than before
+            tempSum+=array[i]
+        if tempSum > currBestSum:	#if tempSum results in a larger sum, then make it the best
+            currBestSum = tempSum
+            left = tempLeft
+            right = i
+    return (left,right,currBestSum)
 
 #TEST SCRIPT
 
@@ -114,30 +126,74 @@ vals3=[1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
 vals4=[10,20,30,40,50]
 vals5=[-1,-1,-1,10000,-1,-1,-1]
 
-print("---TESTING ALGORITHM 1---")
-print(str(enumeration(vals1)))
-print(str(enumeration(vals2)))
-print(str(enumeration(vals3)))
-print(str(enumeration(vals4)))
-print(str(enumeration(vals5)))
+
+mss1=[1, 4, -9, 8, 1, 3, 3, 1, -1, -4, -6, 2, 8, 19, -10, -11]
+mss2=[2, 9, 8, 6, 5, -11, 9, -11, 7, 5, -1, -8, -3, 7, -2]
+mss3=[10, -11, -1, -9, 33, -45, 23, 24, -1, -7, -8, 19]
+mss4=[31,-41, 59, 26, -53, 58, 97, -93, -23, 84]
+mss5=[3, 2, 1, 1, -8, 1, 1, 2, 3]
+mss6=[12, 99, 99, -99, -27, 0, 0, 0, -3, 10]
+mss7=[-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+print("--TESTING ALGORITHM 1---")
+#print(str(enumeration(vals1)))
+#print(str(enumeration(vals2)))
+#print(str(enumeration(vals3)))
+#print(str(enumeration(vals4)))
+#print(str(enumeration(vals5)))
+
+print(str(enumeration(mss1)))
+print(str(enumeration(mss2)))
+print(str(enumeration(mss3)))
+print(str(enumeration(mss4)))
+print(str(enumeration(mss5)))
+print(str(enumeration(mss6)))
+print(str(enumeration(mss7)))
+
+
 
 print("---TESTING ALGORITHM 2---")
-print(str(better_enumeration(vals1)))
-print(str(better_enumeration(vals2)))
-print(str(better_enumeration(vals3)))
-print(str(better_enumeration(vals4)))
-print(str(better_enumeration(vals5)))
-
+#print(str(better_enumeration(vals1)))
+#print(str(better_enumeration(vals2)))
+#print(str(better_enumeration(vals3)))
+#print(str(better_enumeration(vals4)))
+#print(str(better_enumeration(vals5)))
+#
+print(str(better_enumeration(mss1)))
+print(str(better_enumeration(mss2)))
+print(str(better_enumeration(mss3)))
+print(str(better_enumeration(mss4)))
+print(str(better_enumeration(mss5)))
+print(str(better_enumeration(mss6)))
+print(str(better_enumeration(mss7)))
+#
+#
 print("---TESTING ALGORITHM 3---")
-print(str(max_subarray_recursive(vals1,0,10)))
-print(str(max_subarray_recursive(vals2,0,6)))
-print(str(max_subarray_recursive(vals3,0,9)))
-print(str(max_subarray_recursive(vals4,0,4)))
-print(str(max_subarray_recursive(vals5,0,6)))
+# #print(str(max_subarray_recursive(vals1,0,10)))
+# #print(str(max_subarray_recursive(vals2,0,6)))
+# #print(str(max_subarray_recursive(vals3,0,9)))
+# #print(str(max_subarray_recursive(vals4,0,4)))
+# #print(str(max_subarray_recursive(vals5,0,6)))
+
+print(str(max_subarray_recursive(mss1,0,15)))
+print(str(max_subarray_recursive(mss2,0,14)))
+print(str(max_subarray_recursive(mss3,0,11)))
+print(str(max_subarray_recursive(mss4,0,9)))
+print(str(max_subarray_recursive(mss5,0,8)))
+print(str(max_subarray_recursive(mss6,0,9)))
+print(str(max_subarray_recursive(mss7,0,8)))
 
 print("---TESTING ALGORITHM 4---")
-print(str(linear_time(vals1)))
-print(str(linear_time(vals2)))
-print(str(linear_time(vals3)))
-print(str(linear_time(vals4)))
-print(str(linear_time(vals5)))
+# #print(str(linear_time(vals1)))
+# #print(str(linear_time(vals2)))
+# #print(str(linear_time(vals3)))
+# #print(str(linear_time(vals4)))
+# #print(str(linear_time(vals5)))
+
+print(str(linear_time(mss1)))
+print(str(linear_time(mss2)))
+print(str(linear_time(mss3)))
+print(str(linear_time(mss4)))
+print(str(linear_time(mss5)))
+print(str(linear_time(mss6)))
+print(str(linear_time(mss7)))

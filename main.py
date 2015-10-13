fad813a58feb2a2f6a7b7c6da969420e26adf5bc
barlanj <barlanj@onid.oregonstate.edu>
2015-10-13 12:17:31 -0400

    Edited main to write to MSS_Results.txt for all algorithms
    Edited main to write to MSS_Results.txt for all algorithms

--- main.py
+++ main.py
@@ -11,7 +11,7 @@
 #==============================================================================
 import maxSumSubarray
 
-
+testCaseNum = 1
 arrays = []
 with open("MSS_TestProblems.txt", "r") as f:
     for line in f:
@@ -20,13 +20,21 @@ with open("MSS_TestProblems.txt", "r") as f:
         line = line.replace("\n", "")
         mss = line.split(",")
         mss = [int(i) for i in mss]
-        print mss
+        #print "Test Case ", testCaseNum, ":  ", mss
         arrays.append(mss)
+        testCaseNum += 1
 f.close()
 
 print '\n'
-for mss in arrays:
-    print("Algorithm 1 (left,right,sum):" + str(maxSumSubarray.enumeration(mss)))
-    print("Algorithm 2 (left,right,sum):" + str(maxSumSubarray.better_enumeration(mss)))
-    print("Algorithm 3 (left,right,sum):" + str(maxSumSubarray.max_subarray_recursive(mss, 0, len(mss)-1)))
-    print("Algorithm 4 (left,right,sum):" + str(maxSumSubarray.linear_time(mss)) + "\n")
+testCaseNum = 1
+with open("MSS_Results.txt", "w") as f:
+    for mss in arrays:
+        f.write("Test Case: " + str(testCaseNum) + "\n")
+        f.write( str(mss) + "\n" )
+        f.write("--------------------------------\n")
+        f.write("Algorithm 1 (left,right,sum):" + str(maxSumSubarray.enumeration(mss)) + "\n")
+        f.write("Algorithm 2 (left,right,sum):" + str(maxSumSubarray.better_enumeration(mss)) + "\n")
+        f.write("Algorithm 3 (left,right,sum):" + str(maxSumSubarray.max_subarray_recursive(mss, 0, len(mss)-1)) + "\n")
+        f.write("Algorithm 4 (left,right,sum):" + str(maxSumSubarray.linear_time(mss)) + "\n\n\n")
+        testCaseNum += 1
+f.close()
